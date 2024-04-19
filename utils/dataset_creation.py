74f535561e5db0590aa04d50522f4cc97b937e10
Sujeeth13 <sb4839@columbia.edu>
2024-04-19 22:04:21 +0000

    dataset

---
+++ utils/dataset_creation.py
@@ -0,0 +1,38 @@
+import numpy as np
+from PIL import Image
+from skimage import color
+import h5py
+import os
+from tqdm import tqdm
+
+def process_images(image_folder):
+    with h5py.File('../image_data.h5', 'w') as hf:
+        for image_filename in tqdm(os.listdir(image_folder)):
+            try:
+                if image_filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+                    image_path = os.path.join(image_folder, image_filename)
+                    
+                    img = Image.open(image_path)
+                    img_resized = img.resize((256, 256), Image.Resampling.LANCZOS)
+                    img_array = np.array(img_resized)
+                    img_gray = np.array(img_resized.convert('L'))
+                    img_lab = color.rgb2lab(img_resized)
+                    img_a = img_lab[:, :, 1]
+                    img_b = img_lab[:, :, 2]
+
+                    a_bins = np.arange(-100, 100, 10)
+                    b_bins = np.arange(-100, 100, 10)
+                    img_a_binned = np.digitize(img_a, a_bins) - 1
+                    img_b_binned = np.digitize(img_b, b_bins) - 1
+
+                    group = hf.create_group(image_filename)
+                    group.create_dataset('original', data=img_array)
+                    group.create_dataset('grayscale', data=img_gray)
+                    group.create_dataset('A_channel', data=img_a_binned)
+                    group.create_dataset('B_channel', data=img_b_binned)
+            except:
+                pass
+
+if __name__ == '__main__':
+
+    process_images('../data/')
